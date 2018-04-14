a54f1f36fb1d4e1447c8143adbc4d23986de4fae
Colin Brence <cbrence@ramapo.edu>
2018-04-14 17:01:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 var kittens = ['Milo', 'Otis', 'Garfield']
 
-function destructivelyAppendKitten(kittens, 'serPounce') {
+function destructivelyAppendKitten() {
 kittens.push('serPounce')
 }
